1144d3e50581e78634e6fa925c39236c194ba95a
syntel <syntel@PHX440G3-28160N.syntelorg.com>
2018-02-27 13:13:56 -0500

    Group Information

---
+++ src/resume.java
@@ -0,0 +1,57 @@
+public class resume {
+
+    public static void main(String[]args){
+
+        String[]aleya ={ "Name: Aleya", "Education: Georgia State University", "Work Experience: TA"};
+        String[]bryant ={ "Name: Bryant", "Education: Howard University", "Work Experience: Lockheed Martin"};
+        String[] dina ={ "Name: Dina", "Education: Georgia Tech university", "Work Experience: NBC"};
+        String[] branden={ "Name: Branden", "Education: Leo University", "Work Experience: Atlanta Police"};
+        String[]shane ={ "Name: Shane", "Education: Georgia Southern University", "Work Experience: Sales"};
+        String[]rosslyn ={ "Name: Rosslyn", "Education: University of North Texas", "Work Experience: Cartronics"};
+
+        System.out.println("Blue Falcon");
+        System.out.println("---------------");
+        System.out.println("---------------");
+        System.out.println("Team member 1");
+        System.out.println();
+        for (int i=0; i<aleya.length; i++){
+            System.out.println(aleya[i]);
+        }
+
+        System.out.println("---------------");
+        System.out.println("Team member 2");
+        System.out.println();
+        for (int i=0; i<bryant.length; i++){
+            System.out.println(bryant[i]);
+        }
+
+        System.out.println("---------------");
+        System.out.println("Team member 3");
+        System.out.println();
+        for (int i=0; i<dina.length; i++){
+            System.out.println(dina[i]);
+        }
+
+        System.out.println("---------------");
+        System.out.println("Team member 4");
+        System.out.println();
+        for (int i=0; i<branden.length; i++){
+            System.out.println(branden[i]);
+        }
+
+        System.out.println("---------------");
+        System.out.println("Team member 5");
+        System.out.println();
+        for (int i=0; i<shane.length; i++) {
+            System.out.println(shane[i]);
+        }
+
+        System.out.println("---------------");
+
+        System.out.println("Team member 6");
+        System.out.println();
+        for (int i=0; i<rosslyn.length; i++){
+            System.out.println(rosslyn[i]);
+        }
+    }
+}
